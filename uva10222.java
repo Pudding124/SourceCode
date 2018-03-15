f2801bcff847fd5aacafb99fe271739c6d7ef409
許銘仁 <surprised128@gmail.com>
2018-03-15 21:31:23 +0800

    2018/03/15

--- uva10222.java
+++ uva10222.java
@@ -6,18 +6,24 @@ public class uva10222 {
         Scanner input = new Scanner(System.in);
         String keyboard[] = {"q","w","e","r","t","y","u","i","o","p","[","]","\\","a","s","d","f","g","h","j","k","l",";","'","z","x","c","v","b","n","m",",",".","/"};
         while(input.hasNext()){
-            String data = input.next();
+            String data = input.nextLine();
+            data = data.replaceAll(" ","0");
             data = data.toLowerCase();
             for(int j = 0;j<data.length();j++){
                 String tmp = data.substring(j,j+1);
                 for(int i = 0;i<keyboard.length;i++){
                     if(tmp.equals(keyboard[i])){
                         System.out.print(keyboard[i-2]);
+                        break;
+                    }
+                    if(tmp.equals("0")){
+                        System.out.print(" ");
+                        break;
                     }
                 }
 
             }
-            System.out.print(" ");
+            System.out.println();
 
         }
     }
